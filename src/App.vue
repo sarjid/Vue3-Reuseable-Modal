93986b43d61c73af71d7f057f78ff350b1f233dd
sarjid <sarjid777gmail.com>
2023-02-15 01:53:50 +0600

    vue3 Reuseabl Modal

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+import Modal from "./components/Modal.vue";
+const isActiveModal = ref(false);
+
+const toggleModal = () => {
+  isActiveModal.value = !isActiveModal.value;
+};
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-full">
+    <button
+      class="py-2 mt-5 px-4 bg-blue-500 text-white rounded hover:bg-blue-700"
+      @click="toggleModal"
+    >
+      Show Modal
+    </button>
+
+    <Modal :modalStatus="isActiveModal" @close="toggleModal">
+      <template #content>
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <label>Name</label>
+          <input type="text" class="w-full bg-gray-100 p-2 mt-2 mb-3" />
+          <label>Url</label>
+          <input type="text" class="w-full bg-gray-100 p-2 mt-2 mb-3" />
+        </div>
+      </template>
+    </Modal>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,57 @@
+<script setup>
+const props = defineProps({
+  modalStatus: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+const emit = defineEmits(["close"]);
+
+const closeModal = () => {
+  emit("close");
+};
+</script>
+
+<template>
+  <div
+    class="fixed z-10 overflow-y-auto top-0 w-full left-0"
+    v-show="modalStatus"
+  >
+    <div
+      class="flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+    >
+      <div class="fixed inset-0 transition-opacity" @click="closeModal">
+        <div class="absolute inset-0 bg-gray-900 opacity-75" />
+      </div>
+
+      <span class="hidden sm:inline-block sm:align-middle sm:h-screen"
+        >&#8203;</span
+      >
+      <div
+        class="inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="modal-headline"
+      >
+        <slot name="content"></slot>
+        <div class="bg-gray-200 px-4 py-3 text-right">
+          <button
+            type="button"
+            class="py-2 px-4 bg-gray-500 text-white rounded hover:bg-gray-700 mr-2"
+            @click="closeModal"
+          >
+            <i class="fas fa-times"></i> Cancel
+          </button>
+          <button
+            type="button"
+            class="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700 mr-2"
+          >
+            <i class="fas fa-plus"></i> Create
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
